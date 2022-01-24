e70974bbefd43f6c08c0660ff723082cb9d96a33
Andrieli Luci <andrieliluci@gmail.com>
2022-01-24 18:53:53 -0300

    Add GlobalStyle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,8 @@
 import { Box, Button, Text, TextField, Image} from '@skynexui/components'
-import appConfig from '../config.json';
+import appConfig from '../../config.json';
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{` 
+import GlobalStyle from '../styles/global';
 
-            * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            list-style: none;
-            }
-
-            body {
-            font-family: 'Open Sans', sans-serif;
-            }
-
-            /* App fit Height */ 
-            html, body, #__next {
-            min-height: 100vh;
-            display: flex;
-            flex: 1;
-            }
-            
-            #__next {
-            flex: 1;
-            }
-
-            #__next > * {
-            flex: 1;
-            }
-            
-            /* ./App fit Height */ 
-      `}</style>
-    );
-  }
 
 // Component -> Primary Title
 function Title(props) {
