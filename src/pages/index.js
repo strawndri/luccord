3d0be0526aed7301e8b43545ae9ad44d84e9181d
Andrieli Luci <andrieliluci@gmail.com>
2022-01-29 13:25:21 -0300

    Get github informations with fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,84 +8,95 @@ import Container from '../assets/styles/styles';
 
 export default function HomePage() {
 
-  const [userImg, setUserImg] = useState('https://cliply.co/wp-content/uploads/2021/08/372108630_DISCORD_LOGO_BLACK_400.gif');
   const [username, setUsername] = useState('');
+  const [userImage, setUserImage] = useState('/userError.gif');
+  const [userInfo, setUserInfo] = useState([]);
+  const [userIsValid, setUserIsValid] = useState(true);
 
   const routing = useRouter();
   
+  console.log(userInfo)
+
   return (
     <>
     <GlobalStyle />
     <Container>
     
     <Box
-    styleSheet={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      flexDirection: {
-        xs: 'column',
-        sm: 'row',
-      },
-      width: '100%', maxWidth: '700px',
-      borderRadius: '5px', padding: '32px', margin: '16px',
-      boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-      backgroundColor: appConfig.theme.colors.primary["05"],
-    }}
+      styleSheet={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: {
+          xs: 'column',
+          sm: 'row',
+        },
+        width: '100%', maxWidth: '700px',
+        borderRadius: '5px', padding: '32px', margin: '16px',
+        boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+        backgroundColor: appConfig.theme.colors.primary["05"],
+      }}
     >
     {/* Formulário */}
     <Box
-    as="form"
-    onSubmit={function (event) {
-      event.preventDefault();
-      routing.push(`/chat?username=${username}`)
-    }}
-    styleSheet={{
-      display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-      width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
-    }}
+      as="form"
+      onSubmit={function (event) {
+        event.preventDefault();
+        routing.push(`/chat?username=${username}`)
+      }}
+      styleSheet={{
+        display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+        width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+      }}
     >
-    <h1 className="welcome">Bem Vinda(o)!</h1>
-    <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutral["03"]}}>
-    {appConfig.name}
-    </Text>
-    
-    <TextField
-    value={username}
-    placeholder="Digite seu usuário do Github"
-    onChange={function (event) {
-      // Capturar valor
-      const currentValue = event.target.value;
+      <h1 className="welcome">Bem Vinda(o)!</h1>
+      <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutral["03"]}}>
+      {appConfig.name}
+      </Text>
+      
+      <TextField
+      value={username}
+      placeholder="Digite seu usuário do Github"
+      onChange={function (event) {
+        // Capturar valor
+        const currentValue = event.target.value;
 
-      // Alterar o valor da variável
-      setUsername(currentValue)
+        // Alterar o valor da variável
+        setUsername(currentValue)
 
-      if (username.length >= 2) {
-        setUserImg(`https://github.com/${currentValue}.png`)
-      } else {
-        setUserImg('https://cliply.co/wp-content/uploads/2021/08/372108630_DISCORD_LOGO_BLACK_400.gif');
-      }
+        username.length >= 2 
+        ? (
+          setUserImage(`https://github.com/${currentValue}.png`),
+          setUserIsValid(false)
+          // setUserInfo(fetch(`https://api.github.com/users/${currentValue}`))
+        )
+        : (
+          setUserImage('/userError.gif')
+        );
 
-    }}
-    fullWidth
-    textFieldColors={{
-      neutral: {
-        textColor: appConfig.theme.colors.neutral["01"],
-        mainColorHighlight: appConfig.theme.colors.primary["02"],
-        backgroundColor: appConfig.theme.colors.primary["03"],
-      },
-    }}
+        console.log(userInfo)
+
+      }}
+      fullWidth
+      textFieldColors={{
+        neutral: {
+          textColor: appConfig.theme.colors.neutral["01"],
+          mainColorHighlight: appConfig.theme.colors.primary["02"],
+          backgroundColor: appConfig.theme.colors.primary["03"],
+        },
+      }}
     />
-    <Button
-    type='submit'
-    label='Entrar'
-    fullWidth
-    buttonColors={{
-      contrastColor: appConfig.theme.colors.neutral["01"],
-      mainColor: appConfig.theme.colors.secondary["02"],
-      mainColorStrong: appConfig.theme.colors.secondary["03"],
-    }}
-    />
+      <Button
+        disabled={userIsValid}
+        type='submit'
+        label='Entrar'
+        fullWidth
+        buttonColors={{
+          contrastColor: appConfig.theme.colors.neutral["01"],
+          mainColor: appConfig.theme.colors.secondary["02"],
+          mainColorStrong: appConfig.theme.colors.secondary["03"],
+        }}
+      />
     </Box>
     {/* Formulário */}
     
@@ -109,7 +120,7 @@ export default function HomePage() {
       borderRadius: '50%',
       marginBottom: '16px',
     }}
-    src={userImg}
+    src={userImage}
     onChange={function (event) {
       console.log(event)
     }}
