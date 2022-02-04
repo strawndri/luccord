db31c08e6e836198de2d69d81d1344611f46cd77
Andrieli Luci <andrieliluci@gmail.com>
2022-02-04 18:54:31 -0300

    Finish chat section (style)

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import { createClient } from '@supabase/supabase-js';
 
 import { BtnSendSticker } from '../components/BtnSendSticker/BtnSendSticker';
-import Box from '../components/Button/Button';
+import Box from '../styles/Chat';
 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzMxNDM5MiwiZXhwIjoxOTU4ODkwMzkyfQ.Jri-ykLhzA5jByMYR20YuVsFtTfQKLvwo3JoUqfNBnQ';
 const SUPABASE_URL = 'https://xtzudbuzbysbikfxynvn.supabase.co';
@@ -68,26 +68,19 @@ const ChatPage = () => {
     return (
         <>
             <Box>
-                {/* <Box
-                    styleSheet={{
-                        position: 'relative',
-                        display: 'flex',
-                        flex: 1,
-                        height: '80%',
-                        backgroundColor: appConfig.theme.colors.primary["04"],
-                        flexDirection: 'column',
-                        borderRadius: '5px',
-                        padding: '16px',
-                    }}
-                > */}
-                    {/* <MessageList messages={messagesList} /> */}
-                    {/* {messagesList.map((currentMessage) => {
+                <section className="header">
+                    <h3>Logado com </h3>
+                    <a href="/">Sair</a>
+                </section>
+                <section className="chat">
+                    <MessageList messages={messagesList} />
+                    {messagesList.map((currentMessage) => {
                             return (
                                 <li key={currentMessage.id}>
                                     {currentMessage.from}: {currentMessage.txtMessage}
                                 </li>
                             )
-                        })} */}
+                        })}
 
                     {/* <Box
                         as="form"
@@ -134,115 +127,46 @@ const ChatPage = () => {
 
                         <ButtonChat icon="arrowUp" />
                         </Box> */}
-                {/* </Box> */}
+                </section>
             </Box>
         </>
     )
 }
 
-// function Header() {
-//     return (
-//         <>
-//             <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between', color: appConfig.theme.colors.neutral['03'] }} >
-//                 <Text variant='heading5'>
-//                     Chat
-//                 </Text>
-//                 <Button
-//                     variant='tertiary'
-//                     colorVariant='neutral'
-//                     label='Logout'
-//                     href="/"
-//                     styleSheet={{
-//                         color: appConfig.theme.colors.neutral['03'],
-//                         hover: {
-//                             backgroundColor: appConfig.theme.colors.primary['04']
-//                         },
-//                         focus: {
-//                             backgroundColor: appConfig.theme.colors.primary['03']
-//                         }
-//                     }}
-//                 />
-//             </Box>
-//         </>
-//     )
-// }
+function MessageList(props) {
+    return (
+        <ul className="messages__list">
+            {props.messages.map((message) => {
+                return (
+                    <li className='messages__item'
+                        key={message.id}>
 
-// function MessageList(props) {
-//     return (
-//         <Box
-//             tag="ul"
-//             styleSheet={{
-//                 overflow: 'scroll',
-//                 overflowX: 'hidden',
-//                 display: 'flex',
-//                 flexDirection: 'column-reverse',
-//                 flex: 1,
-//                 color: appConfig.theme.colors.neutral["02"],
-//                 marginBottom: '16px',
-//             }}
-//         >
-//             {props.messages.map((message) => {
-//                 return (
-//                     <Text
-//                         key={message.id}
-//                         tag="li"
-//                         styleSheet={{
-//                             borderRadius: '5px',
-//                             padding: '6px',
-//                             marginBottom: '12px',
-//                             fontWeight: 500,
-//                             transition: ".25s",
-//                             hover: {
-//                                 backgroundColor: appConfig.theme.colors.primary["03"],
-//                             }
-//                         }}
-//                     >
-//                         <Box
-//                             styleSheet={{
-//                                 marginBottom: '8px',
-//                             }}
-//                         >
-//                             <Image
-//                                 styleSheet={{
-//                                     width: '20px',
-//                                     height: '20px',
-//                                     borderRadius: '50%',
-//                                     display: 'inline-block',
-//                                     marginRight: '8px',
-//                                 }}
-//                                 src={`https://github.com/${message.from}.png`}
-//                             />
-//                             <Text tag="strong" styleSheet={{ color: appConfig.theme.colors.neutral['01'] }}>
-//                                 {message.from}
-//                             </Text>
-//                             <Text
-//                                 styleSheet={{
-//                                     fontSize: '10px',
-//                                     marginLeft: '8px',
-//                                     color: appConfig.theme.colors.neutral["03"],
-//                                 }}
-//                                 tag="span"
-//                             >
-//                                 {(new Date().toLocaleDateString())}
-//                             </Text>
-//                         </Box>
+                        <div className="messages_user">
+                            <img className="user-image" src={`https://github.com/${message.from}.png`}/>
+                            <h4 className="user-from">{message.from}</h4>
+                                <span className="date">
+                                    {(new Date().toLocaleDateString('pt-BR'))}
+                                </span>
 
-//                         {/* Declarativo */}
-//                         {/* {message.txtMessage.startsWith(':sticker:').toString()} */}
-//                         {message.txtMessage.startsWith(':sticker:')
-//                             ? (
-//                                 <Image src={message.txtMessage.replace(':sticker:', '')} styleSheet={{ width: '10%' }} />
-//                             )
-//                             : (
-//                                 message.txtMessage
-//                             )}
-//                     </Text>
-//                 )
-//             })}
+                            {/* Declarativo */}
+                            {/* {message.txtMessage.startsWith(':sticker:').toString()} */}
+                            {message.txtMessage.startsWith(':sticker:')
+                                ? (
+                                    <p className="message">
+                                        <img className="sticker" src={message.txtMessage.replace(':sticker:', '')} />
+                                    </p>
+                                )
+                                : (
+                                    <p className="message">{message.txtMessage}</p>
+                                )}
+                        </div>
+                    </li>
+                );
+            })}
 
 
-//         </Box>
-//     )
-// }
+        </ul>
+    )
+}
 
 export default ChatPage;
--- src/styles/Chat.js
+++ src/styles/Chat.js
@@ -12,6 +12,76 @@ const Box = styled.div`
     max-width: 95%;
     max-height: 95vh;
     padding: 32px;
+
+    .header {
+        width: 100%; 
+        margin-bottom: 16px; 
+        display: flex; 
+        align-items: center; 
+        justify-content: space-between; 
+        color: ${appConfig.theme.colors.neutral["03"]};
+    }
+
+    .chat {
+        position: relative;
+        display: flex;
+        flex: 1;
+        height: 80%;
+        background-color: ${appConfig.theme.colors.primary["04"]};
+        flex-direction: column;
+        border-radius: 5px;
+        padding: 16px;
+    }
+
+    .messages__list {
+        overflow: scroll;
+        overflow-X: hidden;
+        display: flex;
+        flex-direction: column-reverse;
+        flex: 1;
+        color: ${appConfig.theme.colors.neutral["02"]};
+        margin-bottom: 16px;
+    }
+
+    .messages__item {
+        border-radius: 5px;
+        padding: 6px;
+        margin-bottom: 12px;
+        font-weight: 500;
+        transition: .25s;
+
+        &:hover: {
+            background-color: ${appConfig.theme.colors.primary["03"]};
+        }
+    }
+
+    .user-image {
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        display: inline-block;
+        margin-right: 8px;
+    }
+
+    .user-from {
+        color: ${appConfig.theme.colors.neutral['01']};
+        display: inline-block;
+    }
+
+    .date {
+        font-size: 10px;
+        margin-left: 8px;
+        color: ${appConfig.theme.colors.neutral["03"]};
+    }
+
+    .message {
+        width: 100%;
+        margin: 2em 0;
+    }
+
+    .sticker {
+        width: 10em;
+    }
 `
 
 export default Box;
